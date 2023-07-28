ebc2e0acc1011b876a34909140374629f68e7a5d
ogamache <o.gamache2@gmail.com>
2023-07-28 15:32:14 -0400

    Adapt run.bash file for docker structure

--- run.bash
+++ run.bash
@@ -9,16 +9,16 @@ dataname="MH_01_easy"
 #dataname="V1_03_difficult"
     # run dso
      ./build/bin/dso_dataset \
- 	  files0=/media/sjm/SJM_WIN/AILEARN/SLAM/data_set/${dataname}/mav0/cam0/data \
-	  files1=/media/sjm/SJM_WIN/AILEARN/SLAM/data_set/${dataname}/mav0/cam1/data \
- 	  calib0=/home/sjm/projects/VI-Stereo-DSO/calib/euroc/cam0.txt \
-	  calib1=/home/sjm/projects/VI-Stereo-DSO/calib/euroc/cam1.txt \
-	  T_stereo=/home/sjm/projects/VI-Stereo-DSO/calib/euroc/T_C0C1.txt \
-	  imu_info=/home/sjm/projects/VI-Stereo-DSO/calib/euroc/IMU_info.txt \
-	  groundtruth=/media/sjm/SJM_WIN/AILEARN/SLAM/data_set/${dataname}/mav0/state_groundtruth_estimate0/data.csv \
-	  imudata=/media/sjm/SJM_WIN/AILEARN/SLAM/data_set/${dataname}/mav0/imu0/data.csv \
-	  pic_timestamp=/media/sjm/SJM_WIN/AILEARN/SLAM/data_set/${dataname}/mav0/cam0/data.csv \
-	  pic_timestamp1=/media/sjm/SJM_WIN/AILEARN/SLAM/data_set/${dataname}/mav0/cam1/data.csv \
+ 	  files0=/home/user/data/${dataname}/mav0/cam0/data \
+	  files1=/home/user/data/${dataname}/mav0/cam1/data \
+ 	  calib0=/home/user/repos/calib/euroc/cam0.txt \
+	  calib1=/home/user/repos/calib/euroc/cam1.txt \
+	  T_stereo=/home/user/repos/calib/euroc/T_C0C1.txt \
+	  imu_info=/home/user/repos/calib/euroc/IMU_info.txt \
+	  groundtruth=/home/user/data/${dataname}/mav0/state_groundtruth_estimate0/data.csv \
+	  imudata=/home/user/data/${dataname}/mav0/imu0/data.csv \
+	  pic_timestamp=/home/user/data/${dataname}/mav0/cam0/data.csv \
+	  pic_timestamp1=/home/user/data/${dataname}/mav0/cam1/data.csv \
  	  preset=0 mode=1 \
 	  quiet=1 nomt=1 \
           savefile_tail=nt_${dataname}\
